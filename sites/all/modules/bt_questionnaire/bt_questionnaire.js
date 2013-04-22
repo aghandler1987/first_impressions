6a361712dbfbb1efb537ce13e5c519e731a77cae
Alexander Montgomery <alex@Alexanders-MacBook-Air.local>
2013-04-22 10:07:44 -0400

    beginning of list view

--- sites/all/modules/bt_questionnaire/bt_questionnaire.js
+++ sites/all/modules/bt_questionnaire/bt_questionnaire.js
@@ -1,16 +1,32 @@
 (function ($) {
 Drupal.behaviors.btQuestionnaireQuestions = {
   attach: function(context, settings){
-  var questionForm = $("#bt-questionnaire-add-question-form");
-  $('#add-answer:not(.questionnaireQuestions-processed)', questionForm).addClass('questionnaireQuestions-processed').each(function(){
-    $(this).click(function(){
-       $('div.hidden:first').removeClass('hidden');
-       if ($('div.hidden').length == 0){
-         $(this).remove();
-       }
+    $('#add-answer:not(.questionnaireQuestions-processed)', context).addClass('questionnaireQuestions-processed').each(function(){
+      $(this).click(function(){
+         $('div.hidden:first').removeClass('hidden');
+         if ($('div.hidden').length == 0){
+           $(this).remove();
+         }
+      });
+    });
+  
+    $('#results-wrapper:not(.questionnaireQuestions-processed)', context).addClass('questionnaireQuestions-processed').each(function(){
+      var ajaxImage = null;
+      var wrapperObj = $(this);
+      Drupal.behaviors.btQuestionnaireQuestions.ajaxQList(function(data){
+        ajaxImage = $('.ajax-loader', wrapperObj).detach();
+        wrapperObj.append(data);
+      })
     })
-  });
-
+  },
+  ajaxQList : function(callback){
+    $.ajax({
+      dataType : 'html',
+      url : '/q_list_ajax',
+      success : function(data){
+        callback(data);
+      }
+    })
   }
 }
 })(jQuery);
